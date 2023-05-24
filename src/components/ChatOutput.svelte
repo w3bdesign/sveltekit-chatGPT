bd7643dfe86a6571dd0c80c0ba01842ef12b33c7
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-25 00:39:10 +0200

    Refactor and improve design

---
+++ src/components/ChatOutput.svelte
@@ -0,0 +1,14 @@
+<script>
+	/**
+	 * @type {any[]}
+	 */
+	export let outputText = [];
+</script>
+
+<div class="mt-4">
+{#each outputText as text}
+	<div class="mt-2 border rounded p-4 w-[40rem]">
+		{text}
+	</div>
+{/each}
+</div>
---
+++ src/components/TextArea.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let value = '';
+	export let placeholder = '';
+	export let onInput = () => {};
+</script>
+
+<textarea
+	class="w-[40rem] h-[20rem] border rounded p-2 bg-slate-100"
+	bind:value
+	{placeholder}
+	on:input={onInput}
+/>
