79e51647dafe937ac94da37707ca0dc1ef339723
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-26 00:58:11 +0200

    Improve design

--- src/components/TextArea.svelte
+++ src/components/TextArea.svelte
@@ -6,7 +6,7 @@
 
 <textarea
 	maxlength="4000"
-	class="w-[20rem] md:w-[40rem] h-[15rem] md:h-[20rem] border rounded p-2 bg-slate-50"
+	class="w-[20rem] md:w-[40rem] h-[15rem] md:h-[20rem] border rounded p-4 bg-slate-50"
 	bind:value
 	{placeholder}
 	on:input={onInput}
