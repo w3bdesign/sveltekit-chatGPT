d8063527af87ad2a9947303d2c223ed8f2baecc6
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-26 00:52:48 +0200

    Improve styling and rendering

--- src/components/ChatOutput.svelte
+++ src/components/ChatOutput.svelte
@@ -1,58 +1,74 @@
-<script>
-	import hljs from 'highlight.js';
+<script lang="ts">
+	interface ProcessedText {
+		prettifiedCode: string;
+		links: { url: string; text: string }[];
+		textBeforeCode: string;
+		textAfterCode: string;
+	}
 
+	import hljs from 'highlight.js';
 	import js_beautify from 'js-beautify';
+
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
 
 	import 'highlight.js/styles/github-dark.css';
 
-	import { CodeBlock } from '@skeletonlabs/skeleton';
+	export let outputText: string[] = [];
 
-	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	let processedTexts: ProcessedText[] = [];
 
-	/**
-	 * @type {any[]}
-	 */
-	export let outputText = [];
+	function processText(text: string) {
+		let codeRegex = /```javascript([\s\S]*?)```/;
+		let codeMatch = text.match(codeRegex);
+		let code = codeMatch ? codeMatch[1] : '';
+		let prettifiedCode = js_beautify(code);
 
-	let testText = `[1]: https://stackoverflow.com/questions/45630965/loop-through-list-of-objects-that-contain-a-true-boolean-field-value-using-java "Loop through List of Objects that contain a true Boolean field value ..." [2]: https://stackoverflow.com/questions/30144551/how-do-i-return-2-boolean-values-from-a-while-loop-java "How do i return 2 boolean values from a while loop? Java" [3]: https://stackoverflow.com/questions/26824526/how-to-return-boolean-statement-if-list-has-duplicates "How to return boolean statement if list has duplicates?" [4]: https://stackoverflow.com/questions/59336346/can-you-return-a-boolean-value-in-loop-in-this-condition "Can you return a Boolean value in loop in this condition" [5]: https://stackoverflow.com/questions/54771478/how-to-use-stream-on-method-that-return-boolean-value-with-condition "java - How to use stream on method that return boolean value with ..." [6]: https://www.freecodecamp.org/news/how-to-iterate-over-objects-in-javascript/ "Loop Through an Object in JavaScript – How to Iterate Over an Object in JS" You can loop through the object and return a boolean if any emails have seen set to 0 by using the following code: \`\`\`javascript let mail = { "mail": [ { "id": 1, "sentTo": 9, "time": 0, "news": "test news", "seen": 1, "header": "test header" }, { "id": 2, "sentTo": 9, "time": 0, "news": "test news", "seen": 0, "header": "test header" } ] }; let isSeenSetToZero = false; for (let i = 0; i < mail.mail.length; i++) { if (mail.mail[i].seen === 0) { isSeenSetToZero = true; break; } } console.log(isSeenSetToZero); \`\`\` This will loop through the object and return a boolean if any emails have seen set to 0. In this case, it will return true because the second email has seen set to 0. I hope this helps! Let me know if you have any other questions.`;
+		let newText = text.replace(codeRegex, 'CODE_BLOCK');
+		let linkRegex = /\[\d+\]:\s*(.+?)\s*"(.*?)"\s*/g;
+		let links = [...newText.matchAll(linkRegex)].map((match) => ({
+			url: match[1],
+			text: match[2]
+		}));
+		newText = newText.replace(linkRegex, '');
+		let [textBeforeCode, textAfterCode] = newText.split('CODE_BLOCK');
 
-	let codeRegex = /```javascript([\s\S]*?)```/;
-	let codeMatch = testText.match(codeRegex);
-	let code = codeMatch ? codeMatch[1] : '';
-	let prettifiedCode = js_beautify(code);
+		return { prettifiedCode, links, textBeforeCode, textAfterCode };
+	}
 
-	let newTestText = testText.replace(codeRegex, 'CODE_BLOCK');
-	let linkRegex = /\[\d+\]:\s*(.+?)\s*"(.*?)"\s*/g;
-	let links = [...newTestText.matchAll(linkRegex)].map((match) => ({
-		url: match[1],
-		text: match[2]
-	}));
-	newTestText = newTestText.replace(linkRegex, '');
-	let [textBeforeCode, textAfterCode] = newTestText.split('CODE_BLOCK');
+	$: {
+		processedTexts = outputText.map(processText);
+	}
 
 	storeHighlightJs.set(hljs);
 </script>
 
 <svelte:body on:load={() => hljs.highlightAll()} />
 
-<div class="mt-4 mb-4">
-	<div class="mt-2 border shadow rounded p-4 w-[20rem] md:w-[40rem] relative bg-slate-50">
-		<div class="p-2">
-			{@html textBeforeCode}
-			<div class="py-6">
-				<CodeBlock language="javascript" code={`${prettifiedCode}`} />
-			</div>
-			{@html textAfterCode}
-			<div class="py-2">
-				<h2 class="text-xl py-4 font-bold text-center">Sources</h2>
-				{#each links as link}
-					<div class="py-1 px-2">
-						<a class="chip variant-filled w-full text-sm" href={link.url} target="_blank"
-							>{link.text}</a
-						>
+{#each processedTexts as text}
+	<div class="mt-2 mb-2">
+		<div class="border shadow rounded p-4 w-[20rem] md:w-[40rem] relative bg-slate-50">
+			<div class="p-2">
+				{@html text.textBeforeCode}
+				{#if text.prettifiedCode}
+					<div class="py-6">
+						<CodeBlock language="javascript" code={`${text.prettifiedCode}`} />
 					</div>
-				{/each}
+				{/if}
+				{#if text.textAfterCode} {@html text.textAfterCode} {/if}
+				{#if text.links.length}
+					<div class="py-2">
+						<h2 class="text-xl py-4 font-bold text-center">Sources</h2>
+						{#each text.links as link}
+							<div class="py-1 px-2">
+								<a class="chip variant-filled w-full text-sm" href={link.url} target="_blank"
+									>{link.text}</a
+								>
+							</div>
+						{/each}
+					</div>
+				{/if}
 			</div>
 		</div>
 	</div>
-</div>
+{/each}
