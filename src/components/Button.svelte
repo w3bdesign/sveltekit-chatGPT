31f97c912fefe9ad6954bc7dc684d19c4206b540
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-25 20:38:41 +0200

    Sketelonlabs Codeblock

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,6 +3,6 @@
 	export let buttonAction = () => {};
 </script>
 
-<button class="bg-blue-500 text-white rounded py-2 px-4 mt-4 w-24" on:click={buttonAction}>
+<button type="button" class="btn variant-filled py-2 px-4 mt-4" on:click={buttonAction}>
 	{text}
 </button>
