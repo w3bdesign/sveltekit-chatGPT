def67f26da5dcfafc83ab6a30c6c248b828b1db3
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-26 23:19:41 +0200

    Key for each

--- src/components/ChatOutput.svelte
+++ src/components/ChatOutput.svelte
@@ -34,7 +34,7 @@
 	}}
 />
 
-{#each processedTexts as text}
+{#each processedTexts as text (text.textBeforeCode)}
 	<div class="mt-2 mb-3" in:fly={{ y: 50, duration: 500 }} out:fade>
 		<div class="mt-2 border shadow-md rounded p-4 w-[20rem] md:w-[40rem] relative bg-slate-50">
 			<div class="p-2">
@@ -49,7 +49,7 @@
 					<div class="mt-6"><hr class="h-px bg-gray-300 border-0 dark:bg-gray-700" /></div>
 					<div class="py-2">
 						<h2 class="text-xl py-4 font-bold text-center">Sources</h2>
-						{#each text.links as link}
+						{#each text.links as link (link.text)}
 							{#if link.text}
 								<div class="py-1 px-2">
 									<a class="chip variant-filled w-full text-sm" href={link.url} target="_blank"
