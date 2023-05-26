52b02f5f30db9e747366a3750bd1d7ed19ac2a88
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-26 02:06:28 +0200

    Fade animations

--- src/components/ChatOutput.svelte
+++ src/components/ChatOutput.svelte
@@ -8,6 +8,7 @@
 
 	import hljs from 'highlight.js';
 	import js_beautify from 'js-beautify';
+	import { fade } from 'svelte/transition';
 
 	import { CodeBlock } from '@skeletonlabs/skeleton';
 	import { storeHighlightJs } from '@skeletonlabs/skeleton';
@@ -46,7 +47,10 @@
 <svelte:body on:load={() => hljs.highlightAll()} />
 
 {#each processedTexts as text}
-	<div class="mt-2 mb-3">
+	<div
+		class="mt-2 mb-3 transition duration-500 ease-in-out transform opacity-100"
+		transition:fade={{ delay: 200 }}
+	>
 		<div class="border shadow rounded p-4 w-[20rem] md:w-[40rem] relative bg-slate-50">
 			<div class="p-2">
 				{@html text.textBeforeCode}
