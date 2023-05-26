a42ae8edc3343d7088d832a8247bc5450ebff6c3
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-26 23:05:29 +0200

    Test that API works

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,14 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('Index page has expected h1 text', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+});
+
+test('We get text back when we click the button', async ({ page }) => {
+	await page.goto('/');
+	await page.getByPlaceholder('Type something here to start ...').click();
+	await page.getByPlaceholder('Type something here to start ...').fill('hello');
+	await page.getByRole('button', { name: 'Submit' }).click();
+	await expect(page.getByText('Hello! How can I help you today?')).toBeVisible({ timeout: 10000 });
 });
