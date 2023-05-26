075c88feb31fe2e9a9a24acccc5aa9e58df4a30d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-26 03:18:06 +0200

    Background header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,6 +2,8 @@
 	export let text = '';
 </script>
 
-<h1 class="text-center font-bold text-2xl py-6">
+<div class="bg-white rounded mt-2 mb-4 w-full">
+<h1 class="text-center font-bold text-2xl py-4">
 	{text}
 </h1>
+</div>
