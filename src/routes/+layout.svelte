10d9fedededcf6c69521ca90b9804bdf53068d3d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-27 03:15:28 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,7 @@
 
 	import '../app.css';
 
-	// Your selected Skeleton theme:
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-
-	// This contains the bulk of Skeletons required styles:
-	// NOTE: this will be renamed skeleton.css in the v2.x release.
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 </script>
 
