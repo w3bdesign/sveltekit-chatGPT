2fc8403a145000abd2907bcd2669a7996e846aad
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-27 03:50:39 +0200

    Add Lottie styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -14,7 +14,7 @@
 <div class="flex flex-col items-center justify-center mt-6">
 	<div class="flex flex-col items-center">
 		<Header text="Page not found - 404" />
-		<div class="w-[20rem] md:w-[45rem]">
+		<div class="w-[20rem] md:w-[45rem] rounded shadow-md p-2 bg-white">
 			{#if LottiePlayer}
 				<LottiePlayer
 					src="https://assets4.lottiefiles.com/packages/lf20_zyu0ctqb.json"
@@ -23,7 +23,7 @@
 					controls={false}
 					renderer="svg"
 					background="transparent"
-					height={600}
+					height={700}
 				/>
 			{/if}
 		</div>
