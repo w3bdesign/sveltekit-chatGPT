72cd9b383f92aa70e4707a4c2f72b4688243bebe
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-06-07 01:39:47 +0200

    Streaming response

--- src/components/ChatOutput.svelte
+++ src/components/ChatOutput.svelte
@@ -1,74 +1,67 @@
+<!-- ChatOutput.svelte -->
 <script lang="ts">
-	interface ProcessedText {
-		content: string;
-	}
-
-	interface ProcessedCode {
-		inline?: boolean;
-		type: string;
-		language?: string;
-		code?: string;
-		content?: string;
-	}
-
-	import type { ChatCompletionRequestMessage } from 'openai';
-
-	import { onMount } from 'svelte';
-	import hljs from 'highlight.js';
-	import { fade, fly } from 'svelte/transition';
 	import { CodeBlock } from '@skeletonlabs/skeleton';
-	import { storeHighlightJs } from '@skeletonlabs/skeleton';
-
 	import textStore from '../store/store';
 
-	import { processText, parseTextAndCodeBlocks } from '../utils/functions/functions';
+	function processTextAndCodeBlocks(deltas: Array<Object>) {
+		let content = ''; // initialize an empty string
+		for (let delta of deltas) {
+			// loop over the array of objects
+			content += delta.choices[0].delta.content; // access the content property of each delta object inside the choices array
+		}
+		const regex = /```(?:([\w-]+)\n)?([\s\S]*?)```|`([^`]+)`/g;
+		const blocks = [];
+		let lastIndex = 0;
+		let match;
 
-	import 'highlight.js/styles/github-dark.css';
+		while ((match = regex.exec(content)) !== null) {
+			// Add a text block before the code block
+			if (lastIndex !== match.index) {
+				blocks.push({
+					type: 'text',
+					content: content.slice(lastIndex, match.index)
+				});
+			}
 
-	let processedItems: { text: ChatCompletionRequestMessage; code: ProcessedCode[] }[] = [];
+			if (match[1]) {
+				// Triple backticks
+				blocks.push({ type: 'code', inline: false, language: match[1], code: match[2] });
+			} else if (match[3]) {
+				// Single backtick
+				blocks.push({ type: 'code', inline: true, language: 'plaintext', code: match[3] });
+			}
 
-	storeHighlightJs.set(hljs);
+			lastIndex = regex.lastIndex;
+		}
 
-	onMount(() => {
-		hljs.highlightAll();
-	});
+		// Add the remaining text block after the last code block
+		if (lastIndex < content.length) {
+			blocks.push({
+				type: 'text',
+				content: content.slice(lastIndex)
+			});
+		}
 
-	$: {
-		processedItems = $textStore.outputText.map((text) => {
-			const content = text.choices[0].delta.content;
-			const processedText = processText(content);
-			// get the content from the delta object
-			console.log('processedText', processedText);
-			const processedCode = parseTextAndCodeBlocks(content);
-			console.log('processedCode', processedCode);
-			return { text: processedText, code: processedCode };
-		});
-
-		console.log('processedItems', processedItems);
-		console.log('$textStore.outputText:', $textStore.outputText);
+		return blocks;
 	}
 </script>
 
-<div class="mt-6">
-	{#each processedItems as item}
-		<div class="mt-2 mb-3" in:fly={{ y: 50, duration: 500 }} out:fade>
-			<div class="border shadow-md rounded p-8 w-[20rem] md:w-[45rem] relative bg-slate-50">
-				{#each item.code as block}
-					{#if block.type === 'code'}
-						{#if block.inline}
-							<code class="px-1 py-0.5 m-0 text-sm break-spaces bg-gray-200 rounded-md"
-								>{block.code}</code
-							>
-						{:else}
-							<div class="py-6 px-2">
-								<CodeBlock language={block.language} code={`${block.code}`} />
-							</div>
-						{/if}
-					{:else}
-						{@html block.content}
-					{/if}
-				{/each}
-			</div>
-		</div>
-	{/each}
+<div class="flex flex-col items-center justify-center mt-6">
+	<div class="border shadow-md rounded p-8 w-[20rem] md:w-[45rem] relative bg-slate-50">
+		{#each processTextAndCodeBlocks($textStore.outputText) as block}
+			{#if block.type === 'code'}
+				{#if block.inline}
+					<code class="px-1 py-0.5 m-0 text-sm break-spaces bg-gray-200 rounded-md"
+						>{block.code}</code
+					>
+				{:else}
+					<div class="py-6 px-2">
+						<CodeBlock language={block.language} code={`${block.code}`} />
+					</div>
+				{/if}
+			{:else}
+				{@html block.content}
+			{/if}
+		{/each}
+	</div>
 </div>
