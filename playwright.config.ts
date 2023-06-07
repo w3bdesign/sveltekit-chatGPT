d93a5114a628f1782a52fbb14d58559eec38051c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-06-07 23:20:37 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 5173
+		port: 4173
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
