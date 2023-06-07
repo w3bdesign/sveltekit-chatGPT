0c734ec9e0701316601bf278451b554b7f4b0d79
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-06-07 22:44:35 +0200

    Update test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -7,11 +7,20 @@ test('Index page has expected h1 text', async ({ page }) => {
 
 test('We get text back when we click the button', async ({ page }) => {
 	await page.goto('/');
+
+	await page.exposeFunction('handleSSEMessage', async (data: any) => {
+		if (data.choices && data.choices[0] && data.choices[0].text) {
+			await expect(page.locator('div[data-testid="output-text-container"]')).toContainText(
+				data.choices[0].text,
+				{ timeout: 10000 }
+			);
+		}
+	});
+
 	await page.getByPlaceholder('Type something here to start ...').click();
 	await page.getByPlaceholder('Type something here to start ...').fill('hello');
 	await page.getByRole('button', { name: 'Submit' }).click();
 
-	// Wait for the expected text to be visible on the page
 	await page.waitForFunction(
 		() => {
 			const el = document.querySelector('div[data-testid="output-text-container"]');
