27cdf0bdc3c2c1a8d86d770041df286aebe5c779
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-06-07 22:35:28 +0200

    Update test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -10,5 +10,15 @@ test('We get text back when we click the button', async ({ page }) => {
 	await page.getByPlaceholder('Type something here to start ...').click();
 	await page.getByPlaceholder('Type something here to start ...').fill('hello');
 	await page.getByRole('button', { name: 'Submit' }).click();
+
+	// Wait for the expected text to be visible on the page
+	await page.waitForFunction(
+		() => {
+			const el = document.querySelector('div[data-testid="output-text-container"]');
+			return el && el.textContent?.includes('Hello! How can I help you today?');
+		},
+		{ timeout: 10000 }
+	);
+
 	await expect(page.getByText('Hello! How can I help you today?')).toBeVisible({ timeout: 10000 });
 });
