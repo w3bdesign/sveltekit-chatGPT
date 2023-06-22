332d2bbfdb08b45a82f6d5dcd087b0520f8d4978
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-06-22 17:55:18 +0200

    Test chatGPT output

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,30 @@ test('Index page has expected h1 text', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'GPT-4 Chat' })).toBeVisible();
 });
+
+test('GPT Chat and text submission', async ({ page }) => {
+	// Navigate to the page with the GPT Chat component
+	await page.goto('/');
+  
+	// Ensure the page title is correct
+	const pageTitle = await page.title();
+	expect(pageTitle).toBe('Main - GPT-4 Chat');
+  
+	// Type user input into the textarea
+	const userInput = 'Hi';
+	await page.fill('textarea', userInput);
+  
+	// Wait for the AI response and check that the loading spinner is hidden
+	await page.getByPlaceholder('Type something here to start ...');
+  
+	// Click the submit button
+	await page.getByRole('button', { name: 'Submit' }).click();
+  
+	// Wait for the output message to appear on the page
+	await page.waitForSelector('text=Hello! How can I help you today?');
+  
+	// Check if there is at least one output message
+	const outputMessage = await page.getByText('Hello! How can I help you today?');
+  
+	expect(outputMessage).toBeVisible();
+  });
