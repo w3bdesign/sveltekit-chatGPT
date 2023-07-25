540ca47dd06d8091da049d9b559aa744b3df803b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-07-26 00:52:09 +0200

    Update ApiStatus.svelte

--- src/components/ApiStatus.svelte
+++ src/components/ApiStatus.svelte
@@ -1,50 +1,66 @@
 <script lang="ts">
 	import { SSE } from 'sse.js';
 	import { onMount } from 'svelte';
-	import { fade } from 'svelte/transition';
-	let statusMessage = 'Connecting to API';
+	import { writable } from 'svelte/store';
+
+	let statusMessage = writable('Connecting to API');
+	let showAlert = writable(true);
 
 	const checkApiConnection = async () => {
+		let isResponseReceived = false;
 		try {
 			const eventSource = new SSE('/api/gpt', {
 				headers: { 'Content-Type': 'application/json' },
 				payload: JSON.stringify({
-					messages: [{ role: 'user', content: 'hello' }],
+					messages: [{ role: 'user', content: 'Hello' }],
 					model: 'gpt-4'
 				})
 			});
 
-			eventSource.addEventListener('error', (event: any) => {
+			eventSource.addEventListener('error', async (event: any) => {
 				console.error('Error event:', event);
-				statusMessage = 'Failed to connect to API';
+				statusMessage.set('Failed to connect to API');
+				isResponseReceived = true;
 				eventSource.close();
 			});
 
-			eventSource.addEventListener('message', (event: { data: any }) => {
+			eventSource.addEventListener('message', async (event: { data: any }) => {
 				console.log('Message event data:', event.data);
-				statusMessage = 'Connected to API';
+				statusMessage.set('Connected to API');
+				isResponseReceived = true;
 				eventSource.close();
 			});
 
 			await eventSource.stream();
 		} catch (error) {
 			console.error('Exception:', error);
-			statusMessage = 'Failed to connect to API';
+			statusMessage.set('Failed to connect to API');
+			await updateAlert();
 		}
 	};
 
 	onMount(() => {
 		checkApiConnection();
 	});
+
+	const updateAlert = async () => {
+		// Hide the alert
+		showAlert.set(false);
+		// Wait for the hide transition to complete
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+		// Show the alert again
+		showAlert.set(true);
+	};
 </script>
 
 <div
-	class="alert w-full mt-4 mb-4
-        {statusMessage === 'Connected to API'
+	class="alert w-full mt-1 mb-5 transition-opacity duration-200
+        {$statusMessage === 'Connected to API'
 		? 'variant-filled-primary'
-		: statusMessage === 'Connecting to API'
+		: $statusMessage === 'Connecting to API'
 		? 'variant-filled-warning'
-		: 'variant-filled-error'}"
+		: 'variant-filled-error'}
+        {$showAlert ? 'opacity-100' : 'opacity-0'}"
 >
-	<p class="text-center mx-auto">{statusMessage}</p>
+	<p class="text-center mx-auto">{$statusMessage}</p>
 </div>
