f4e67105ada68625f9afd7842bd76a988b487b7e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-07-27 05:46:53 +0200

    Update ChatOutput.svelte

--- src/components/ChatOutput.svelte
+++ src/components/ChatOutput.svelte
@@ -72,7 +72,7 @@
 	}
 </script>
 
-<div class="w-full" data-testid="ai-response-container">
+<div class="w-full sm:w-auto" data-testid="ai-response-container">
 	<div class="mt-2 mb-3" in:fly={{ y: 50, duration: 500 }} out:fade>
 		<div class="border shadow-md rounded p-2 w-full md:w-[45rem] relative bg-white">
 			<div class="w-full flex justify-end">
