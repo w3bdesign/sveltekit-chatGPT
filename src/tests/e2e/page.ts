fbeeec4828a49df85fcf12cbe201dbd353f0e2d7
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-08-01 22:36:28 +0200

    Playwright move test

---
+++ src/tests/e2e/page.ts
@@ -0,0 +1,30 @@
+import { expect, test } from '@playwright/test';
+
+test('GPT Chat and text submission', async ({ page }) => {
+	// Navigate to the page with the GPT Chat component
+	await page.goto('/');
+
+	await expect(page.getByRole('heading', { name: 'GPT-4 Chat' })).toBeVisible();
+
+	// Ensure the page title is correct
+	const pageTitle = await page.title();
+	expect(pageTitle).toBe('Main - GPT-4 Chat');
+
+	// Type user input into the textarea
+	const userInput = 'Hi';
+	await page.fill('textarea', userInput);
+
+	// Wait for the AI response and check that the loading spinner is hidden
+	await page.getByPlaceholder('Type something here to start ...');
+
+	// Click the submit button
+	await page.getByRole('button', { name: 'Submit' }).click();
+
+	// Wait for the output message to appear on the page
+	await page.waitForSelector('[data-testid="ai-response-container"]', { timeout: 10000 });
+
+	// Check if there is at least one output message container
+	const outputMessageContainer = await page.getByTestId('ai-response-container');
+
+	expect(outputMessageContainer).toBeVisible;
+});
