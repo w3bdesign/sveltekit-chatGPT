0c69e299645b6e1276252bd2c7defd69c5fa4106
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-08-01 21:18:02 +0200

    Clipboard

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import type { MouseEventHandler } from 'svelte/elements';
+
 	/**
 	 * Customizable button that allows the user to define its action, type, and width.
 	 */
@@ -6,12 +8,14 @@
 	export let buttonType = 'filled';
 	export let buttonWidth = '8rem';
 	export let isDisabled = false;
+	export let buttonAction: MouseEventHandler<HTMLButtonElement> | undefined;
 </script>
 
 <button
 	type="submit"
 	class={`btn variant-${buttonType} py-2 w-[${buttonWidth}]`}
 	disabled={isDisabled}
+	on:click={buttonAction}
 >
 	<slot />
 </button>
