6d14f096eb28afc7dcbfa57a634f63e47b5f075a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-08-01 22:25:51 +0200

    Fix Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { resolve } from 'node:path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['tests/unit/components/*.{test,spec}.{js,ts}'],
+		include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
 		globals: true,
 		environment: 'jsdom'
+	},
+	resolve: {
+		alias: [
+			{ find: '$', replacement: resolve(__dirname, './src') },
+			{ find: '$components', replacement: resolve(__dirname, './src/components') }
+		]
 	}
 });
