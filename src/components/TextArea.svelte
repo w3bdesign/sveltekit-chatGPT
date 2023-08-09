02c0bf359056870847be9cdfc6b5d9ddcaca3e20
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2023-08-09 06:12:12 +0200

    Update TextArea.svelte

--- src/components/TextArea.svelte
+++ src/components/TextArea.svelte
@@ -16,7 +16,7 @@
 <textarea
 	{id}
 	maxlength="9000"
-	class="w-full md:w-[45rem] h-[15rem] md:h-[20rem] border rounded p-4 bg-white shadow-md mb-4"
+	class="w-[20rem] md:w-[45rem] h-[15rem] md:h-[20rem] border rounded p-4 bg-white shadow-md mb-4"
 	disabled={isLoading}
 	bind:value
 	{placeholder}
